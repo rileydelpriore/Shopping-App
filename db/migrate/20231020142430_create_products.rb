149af1d196a92555fd98203fafadcf1b12d6c80b
Riley Del Priore <rdelpriore@colgate.edu>
2023-10-20 10:25:48 -0400

    Model Created

---
+++ db/migrate/20231020142430_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :seller
+      t.string :size
+      t.text :description
+      t.string :condition
+      t.string :brand
+      t.decimal :price, precision: 10, scale: 2
+      t.decimal :original_price, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
