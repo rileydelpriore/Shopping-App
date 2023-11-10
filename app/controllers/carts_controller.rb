d4e8274f6013625ceaf35917e8bc5d826bd938ee
Riley Del Priore <rdelpriore@colgate.edu>
2023-11-10 14:55:46 -0500

    MVC updated

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,10 @@
 class CartsController < ApplicationController
   def show
-    @cart_product_ids = session[:cart] || []
-    @cart_products = Product.where(id: @cart_product_ids)
-    @total_price = calculate_total_price(@cart_products)
+    @cart = Cart.new(session[:cart] || [])
+    @total_price = @cart.total_price
+    @cart_products = @cart.products
   end
-
-  def calculate_total_price(products)
-    products.sum(&:price)
-  end
-
+  
   def remove_from_cart
     product = Product.find(params[:id])
     session[:cart].delete(product.id) if session[:cart]
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,19 @@ class Cart < ApplicationRecord
   has_one :checkout
   has_many :cart_products
   has_many :products, through: :cart_products
+
+  attr_accessor :product_ids
+
+  # initialize user data -- to be changed once login is implemented
+  def initialize(product_ids = [])
+    self.product_ids = product_ids
+  end
+
+  def products
+    Product.where(id: product_ids)
+  end
+
+  def total_price
+    products.sum(&:price)
+  end
 end
